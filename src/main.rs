1dbb4a59c81d54447f958871fc629063a3dd62b7
Rudyard Richter <richter3141@gmail.com>
2022-10-03 14:33:08 -0600

    Add vga_buffer.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![cfg_attr(not(test), no_std)]
 #![no_main]
 
+extern crate volatile;
+
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello, world!";
-static COLOR: u8 = 0xb; // cyan
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -13,14 +14,15 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
+    /*
     for (i, byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = *byte;
             *vga_buffer.offset(i as isize * 2 + 1) = COLOR;
         }
     }
+    */
+    vga_buffer::print_something();
 
     loop {}
 }
