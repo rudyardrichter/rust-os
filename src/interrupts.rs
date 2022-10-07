183fe19931f55d0432cc53dc48d9e8888e14da97
Rudyard Richter <richter3141@gmail.com>
2022-10-07 12:27:41 -0600

    Demonstrate page fault handling

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,8 +1,8 @@
-use crate::gdt;
+use crate::{gdt, hlt_loop};
 use crate::{print, println};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 use x86_64::structures::tss::TaskStateSegment;
 use x86_64::VirtAddr;
 
@@ -40,6 +40,7 @@ lazy_static! {
         }
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 }
@@ -111,6 +112,20 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) -> () {
+    use x86_64::registers::control::Cr2;
+    // The CPU automatically sets the CR2 register when a page fault occurs.
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed address: {:?}", Cr2::read());
+    println!("Error code:       {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 #[test_case]
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,13 @@ use rust_os::{hlt_loop, println};
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     rust_os::init();
+
+    // Demonstrate page fault
+    let ptr = 0xdeadbeef as *mut u32;
+    unsafe {
+        *ptr = 42;
+    }
+
     hlt_loop();
 }
 
